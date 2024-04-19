ee3253bc3aa8eb9d0fa46ee27404f899b8fb5909
Paul Spooner <dudecon@hotmail.com>
2024-04-19 15:17:26 -0700

    Library button setup
    wip

---
+++ BookshelfBookButton.gd
@@ -0,0 +1,15 @@
+extends Button
+
+var booklist
+var this_ID
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	booklist = %Library.all_books
+	this_ID = len(booklist)
+	booklist.append( "this is book " + str(this_ID))
+
+
+func _on_pressed():
+	print(booklist[this_ID]," out of a total of ", len(booklist))
+
