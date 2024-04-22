3649b6e7a6058ca428f7aaeecd9b2febd4f91163
TheHumblePaladin <50687980+TheHumblePaladin@users.noreply.github.com>
2024-04-21 23:40:11 -0700

    socail battery

--- Escape to Social.gd
+++ Escape to Social.gd
@@ -3,12 +3,15 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#visible = false
+	visible = false
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if %Social.social_battery > 30:
+		visible = true
+
 	pass
 
 func _pressed():
