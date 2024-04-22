1f9aa2da9f6ae5e4b5bd425fab5993828f5f530a
Paul Spooner <dudecon@hotmail.com>
2024-04-21 23:23:46 -0700

    button visible for testing

--- Escape to Social.gd
+++ Escape to Social.gd
@@ -3,7 +3,7 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +12,4 @@ func _process(delta):
 
 func _pressed():
 	%Social.visible = true
-	visible = false
+	#visible = false
