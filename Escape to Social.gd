5007a38da20dc4f9df0e21bee349775ef87bf805
Paul Spooner <dudecon@hotmail.com>
2024-04-21 23:34:19 -0700

    social cards fixed

--- Escape to Social.gd
+++ Escape to Social.gd
@@ -4,6 +4,7 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#visible = false
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
