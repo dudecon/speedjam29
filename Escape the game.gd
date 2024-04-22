de4c7854388789ba63515cf649850d7d82b3c7fb
Paul Spooner <dudecon@hotmail.com>
2024-04-22 10:30:57 -0700

    escape music

--- Escape the game.gd
+++ Escape the game.gd
@@ -16,3 +16,5 @@ func _pressed():
 	$".".owner.escaped_score = $".".owner.current_score
 	%"Final Score".text = str($".".owner.escaped_score)
 	%Submit.visible = true
+	$"Escaped 1".play(0)
+	text = "ESCAPED!"
