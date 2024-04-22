066c6438136c5a37713729d48cceb69d7efe1191
Paul Spooner <dudecon@hotmail.com>
2024-04-22 11:35:25 -0700

    help hidden

--- Help Button.gd
+++ Help Button.gd
@@ -1,13 +1,19 @@
 extends Button
 
+var usersettings
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	usersettings = get_node("/root/UserSettings")
+	print(name)
+	if usersettings.helphidden[name]:
+		modulate[3] = 0.2
 
 func _toggled(toggled_on):
 	$Control.visible = toggled_on
 	if !toggled_on:
 		modulate[3] = 0.2
+		usersettings.helphidden[name] = true
 	else:
 		modulate[3] = 1
+		usersettings.helphidden[name] = false
--- user_settings.gd
+++ user_settings.gd
@@ -3,4 +3,4 @@ extends Node
 var playername = "Unknown"
 var player_leaderboard_ID = ""
 
-var musicplayhead = 0
+var helphidden = {"Social Help Button":false,"Help Button":false}
